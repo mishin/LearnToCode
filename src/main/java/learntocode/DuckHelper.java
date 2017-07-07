ea840ea12f47a30a8546498dec97e5e3c4c2f173
Nikolay Mishin <mi@ya.ru>
2017-07-07 18:58:34 +0300

    Update DuckHelper.java

--- src/main/java/learntocode/DuckHelper.java
+++ src/main/java/learntocode/DuckHelper.java
@@ -1,9 +1,7 @@
 package headfirst.designpatterns.iterator.dinermergercafe;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
@@ -31,5 +29,67 @@ public class DuckHelper {
 
         Supplier<ArrayList> methodRef4 = ArrayList::new;
         Supplier<ArrayList> lambda4 = () -> new ArrayList();
+
+        List<String> list = new ArrayList<>();
+        list.add("Magician");
+        list.add("Assistant");
+        System.out.println(list); // [Magician, Assistant]
+        list.removeIf(s -> s.startsWith("A"));
+        System.out.println(list); // [Magician]
+
+        List<Integer> list1 = Arrays.asList(1, 2, 3);
+        list1.replaceAll(x -> x * 2);
+        System.out.println(list1); // [2, 4, 6]
+
+        List<String> cats = Arrays.asList("Annie", "Ripley");
+        for (String cat : cats)
+            System.out.println(cat);
+
+        cats.forEach(c -> System.out.println(c));
+        cats.forEach(System.out::println);
+
+        Map<String, String> favorites1 = new HashMap<>();
+        favorites1.put("Jenny", "Bus Tour");
+        favorites1.put("Jenny", "Tram");
+        System.out.println(favorites1); // {Jenny=Tram}
+
+/*        Map<String, String> favorites = new HashMap<>();
+        favorites.put("Jenny", "Bus Tour");
+        favorites.put("Tom", null);
+        favorites.putIfAbsent("Jenny", "Tram");
+        favorites.putIfAbsent("Sam", "Tram");
+        favorites.putIfAbsent("Tom", "Tram");
+        System.out.println(favorites); // {Tom=Tram, Jenny=Bus Tour, Sam=Tram}*/
+
+/*        BiFunction<String, String, String> mapper = (v1, v2)
+                -> v1.length() > v2.length() ? v1 : v2;
+
+        Map<String, String> favorites = new HashMap<>();
+        favorites.put("Jenny", "Bus Tour");
+        favorites.put("Tom", "Tram");
+
+        String jenny = favorites.merge("Jenny", "Skyride", mapper);
+        String tom = favorites.merge("Tom", "Skyride", mapper);
+
+        System.out.println(favorites); // {Tom=Skyride, Jenny=Bus Tour}
+        System.out.println(jenny); // Bus Tour
+        System.out.println(tom); // Skyride*/
+
+  /*      BiFunction<String, String, String> mapper = (v1, v2) -> v1.length() >
+                v2.length() ? v1 : v2;
+        Map<String, String> favorites = new HashMap<>();
+        favorites.put("Sam", null);
+        favorites.merge("Tom", "Skyride", mapper);
+        favorites.merge("Sam", "Skyride", mapper);
+        System.out.println(favorites); // {Tom=Skyride, Sam=Skyride}*/
+
+        BiFunction<String, String, String> mapper = (v1, v2) -> null;
+        Map<String, String> favorites = new HashMap<>();
+        favorites.put("Jenny", "Bus Tour");
+        favorites.put("Tom", "Bus Tour");
+        favorites.merge("Jenny", "Skyride", mapper);
+        favorites.merge("Sam", "Skyride", mapper);
+        System.out.println(favorites); // {Tom=Bus Tour, Sam=Skyride}
+
     }
-}--154 --157-158
+}
