195c2f54f11a2cbd3b58f8bd92617e8c1c2891cd
Nikolay Mishin <mi@ya.ru>
2017-07-07 18:38:44 +0300

    Create DuckHelper.java

---
+++ src/main/java/learntocode/DuckHelper.java
@@ -0,0 +1,35 @@
+package headfirst.designpatterns.iterator.dinermergercafe;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class DuckHelper {
+    public static int compareByWeight(DuckDuck d1, DuckDuck d2) {
+        return d1.getWeight() - d2.getWeight();
+    }
+
+    public static int compareByName(DuckDuck d1, DuckDuck d2) {
+        return d1.getName().compareTo(d2.getName());
+    }
+
+    public static void main(String[] args) {
+        Comparator<DuckDuck> byWeight = DuckHelper::compareByWeight;
+        Consumer<List<Integer>> methodRef1 = Collections::sort;
+        Consumer<List<Integer>> lambda1 = l -> Collections.sort(l);
+
+        String str = "abc";
+        Predicate<String> methodRef2 = str::startsWith;
+        Predicate<String> lambda2 = s -> str.startsWith(s);
+
+        Predicate<String> methodRef3 = String::isEmpty;
+        Predicate<String> lambda3 = s -> s.isEmpty();
+
+        Supplier<ArrayList> methodRef4 = ArrayList::new;
+        Supplier<ArrayList> lambda4 = () -> new ArrayList();
+    }
+}--154
